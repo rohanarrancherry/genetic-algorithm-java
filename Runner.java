1d76ad5b5266a181ed66596854bdc01d7f4e7f5d
sumitra <sumitragauda@gmail.com>
2021-12-08 16:56:52 +0000

    onepointcrossover added

--- Runner.java
+++ Runner.java
@@ -3,7 +3,7 @@ public class Runner {
     public static void main(String args[]){
         GA geneticAlgo = new GA();
         geneticAlgo.setSelection(String.valueOf(Utilities.SELECTIONS.RankSelection));
-        geneticAlgo.setCrossover(String.valueOf(Utilities.CROSSOVERS.CustomCrossover));
+        geneticAlgo.setCrossover(String.valueOf(Utilities.CROSSOVERS.OnePointCrossover));
         geneticAlgo.setMutation(String.valueOf(Utilities.MUTATIONS.CustomMutation));
         geneticAlgo.run();
     }
